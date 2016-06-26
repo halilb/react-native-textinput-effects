12ba40b61e34400b20eb899a2e22f7ec1efcd0b1
Halil Bilir <hbilir@gmail.com>
2016-06-26 23:51:47 +0300

    created Hideo

--- Example/TextInputEffectsExample.js
+++ Example/TextInputEffectsExample.js
@@ -4,6 +4,8 @@ import {
   StyleSheet,
 } from 'react-native';
 
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+
 import {
   Kaede,
   Hoshi,
@@ -11,6 +13,7 @@ import {
   Isao,
   Madoka,
   Akira,
+  Hideo,
 } from 'react-native-textinput-effects';
 
 export default class TextInputEffectsExample extends Component {
@@ -19,6 +22,7 @@ export default class TextInputEffectsExample extends Component {
     return (
       <ScrollView style={styles.container} keyboardDismissMode={'on-drag'}>
         <Kaede
+          style={styles.input}
           label={'Number'}
           labelStyle={{
             color: '#990fe2',
@@ -31,26 +35,35 @@ export default class TextInputEffectsExample extends Component {
           keyboardType="numeric"
         />
         <Hoshi
+          style={styles.input}
           label={'Street'}
           backgroundColor={'#F9F7F6'}
           borderColor={'#00ffaa'}
         />
         <Jiro
+          style={styles.input}
           label={'Cat\'s name'}
           borderColor={'#00ffaa'}
         />
         <Isao
+          style={styles.input}
           label={'Middle name'}
           borderColor={'#da7071'}
         />
+        <Akira
+          style={styles.input}
+          label={'Maiden Name'}
+          borderColor={'#7A7593'}
+        />
         <Madoka
-          style={{ marginTop: 8 }}
+          style={styles.input}
           label={'Weight'}
           borderColor={'#7A7593'}
         />
-        <Akira
-          label={'Maiden Name'}
-          borderColor={'#7A7593'}
+        <Hideo
+          iconClass={FontAwesomeIcon}
+          iconName={'envelope'}
+          iconColor={'white'}
         />
       </ScrollView>
     );
@@ -62,5 +75,8 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 24,
     backgroundColor: '#F9F7F6',
+  },
+  input: {
+    marginTop: 4,
   },
 });
