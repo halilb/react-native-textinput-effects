5c58ddf1e2296bac77ea9e390dd11ea81b738bbc
Halil Bilir <hbilir@gmail.com>
2017-11-19 20:17:57 +0300

    upgraded example project to react-native 0.50

--- Example/index.ios.js
+++
@@ -1,7 +0,0 @@
-import React, {
-  AppRegistry,
-} from 'react-native';
-
-import TextInputEffectsExample from './TextInputEffectsExample';
-
-AppRegistry.registerComponent('TextInputEffects', () => TextInputEffectsExample);
